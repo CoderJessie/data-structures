79ad48958f9925a9f640f15c1e1383b78eebc616
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-10 18:55:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const restaurant = {
+	name: 'Classical Italiano',
+	location: 'Via Angelo Tavanti 23, Firenze, Italy',
+	categories: ['Italian', 'Bruschetta', 'vegetarian', 'Organic'],
+	starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+	mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
