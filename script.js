129c90bf9ebaf6fe048db5568ad3a23920db2b8f
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-12 00:10:54 +0100

    short circuiting

--- script.js
+++ script.js
@@ -41,8 +41,24 @@ const restaurant = {
 			`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
 		);
 	},
+
+	orderPizza: function (mainIngredient, ...otherIngredients) {
+		console.log(mainIngredient);
+		console.log(otherIngredients);
+	},
 };
 
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+/*
+/////////////////////////////////
+// Rest Pattern and Parameters
 // 1) Destructuring
 
 // SPREAD, because its on the right side of =
@@ -75,7 +91,9 @@ add(8, 2, 5, 3, 2, 1, 4);
 const x = [23, 5, 7];
 add(...x);
 
-/*
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms'); 
+
 ///////////////////////////////
 // Spread Operator
 const arr = [7, 8, 9];
