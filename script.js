7ef5b27d20f5e4b5a23312de160dd0732f300dcf
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-11 14:39:10 +0100

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,20 @@
 const restaurant = {
 	name: 'Classical Italiano',
 	location: 'Via Angelo Tavanti 23, Firenze, Italy',
-	categories: ['Italian', 'Bruschetta', 'vegetarian', 'Organic'],
+	categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
 	starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
 	mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// With destructuring we can actually declare all 3 variables at the same time
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
