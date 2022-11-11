e04a259e1b07fffebe5725be743519b6ac82e9d3
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-11 15:49:15 +0100

    destructuring objects

--- script.js
+++ script.js
@@ -6,12 +6,33 @@ const restaurant = {
 	categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
 	starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
 	mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+	openingHours: {
+		thu: {
+			open: 12,
+			close: 22,
+		},
+		fri: {
+			open: 11,
+			close: 23,
+		},
+		sat: {
+			open: 0, // Open 24 hours
+			close: 24,
+		},
+	},
 
 	order: function (starterIndex, mainIndex) {
 		return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
 	},
 };
 
+// DESTRUCTURING OBJECTS
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+/*
+/////////////////////////////
+/DESTRUCTURING ARRAYS
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -39,7 +60,7 @@ console.log(main, secondary);
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
-// What if we have a nested array
+// Nested destructuring
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 // console.log(i, j);
@@ -49,3 +70,8 @@ const [i, , [j, k]] = nested;
 console.log(i, j, k);
 // const [i, j, [k, l]] = nested;
 // console.log(i, j, k, l);
+
+// Default Values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
