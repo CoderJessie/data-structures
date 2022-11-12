12c936f89bfdf45869f2a6b9d300b53bf7eac6c7
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-12 21:54:23 +0100

    short circuiting

--- script.js
+++ script.js
@@ -56,6 +56,10 @@ console.log(undefined || null);
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
 /*
 /////////////////////////////////
 // Rest Pattern and Parameters
