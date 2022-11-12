467c2c0d1a07c05587bdc9dba3f4c0519aa66c92
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-12 22:10:57 +0100

    short circuiting

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ const restaurant = {
 	},
 };
 
+console.log('---- OR ----');
 // Use ANY data type, return ANY data type, short-circuiting
 console.log(3 || 'Jonas');
 console.log('' || 'Jonas');
@@ -56,9 +57,18 @@ console.log(undefined || null);
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-restaurant.numGuests = 23;
+// restaurant.numGuests = 23;
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
 
 /*
 /////////////////////////////////
