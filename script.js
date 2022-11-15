8155171026527fbc2adcbc37eef3d8e37138d828
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-15 02:46:41 +0100

    code challenge 1

--- script.js
+++ script.js
@@ -60,8 +60,16 @@ Suppose we get data from a web service about a certain game (below). In this cha
 2. The first player in any player array is the goalkeeper and the others are field players. for Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. 
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals who were scored (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which yteam is likely to win, WITHOUT using an if/else statement or the ternary operator.
 
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Levandowski' and 'Kimich'. Then, call the function again with players from game.scorerd
+
+GOOD LUCK
+*/
+
+const game = {};
 
 ///////////////////////////
 // Nullish Coalescing Operator
@@ -140,7 +148,7 @@ const x = [23, 5, 7];
 add(...x);
 
 restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
-restaurant.orderPizza('mushrooms'); 
+restaurant.orderPizza('mushrooms');
 
 /*
 ///////////////////////////////
