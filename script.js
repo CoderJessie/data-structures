4690e196a84ea3f321fada3f4af6a14b77ffa71a
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-16 10:28:53 +0100

    code challenge #1

--- script.js
+++ script.js
@@ -86,7 +86,28 @@ const game = {
 			'Gnarby',
 			'Lewandowski',
 		],
+		[
+			'Burki',
+			'Schulz',
+			'Hummels',
+			'Akanji',
+			'Hakimi',
+			'Weigl',
+			'Witsel',
+			'Hazard',
+			'Brandt',
+			'Sancho',
+			'Gotze',
+		],
 	],
+	score: '4:0',
+	scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+	date: 'Nov 9th, 2037',
+	odds: {
+		team1: 1.33,
+		x: 3.25,
+		team2: 6.5,
+	},
 };
 
 ///////////////////////////
