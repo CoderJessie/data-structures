edea381c6996d71ee9397f7333e8611769ee71e6
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-19 03:18:41 +0100

    enhanced object literals

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const openingHours = {
+const hours = {
 	thu: {
 		open: 12,
 		close: 22,
@@ -23,35 +23,31 @@ const restaurant = {
 	mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
 	// ES6 enhanced object literals
-	openingHours,
+	hours,
 
-	order: function (starterIndex, mainIndex) {
+	order(starterIndex, mainIndex) {
 		return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
 	},
 
-	orderDelivery: function ({
-		starterIndex = 1,
-		mainIndex = 0,
-		time = '20:00',
-		address,
-	}) {
+	orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
 		console.log(
 			`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
 		);
 	},
 
-	orderPasta: function (ing1, ing2, ing3) {
+	orderPasta(ing1, ing2, ing3) {
 		console.log(
 			`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
 		);
 	},
 
-	orderPizza: function (mainIngredient, ...otherIngredients) {
+	orderPizza(mainIngredient, ...otherIngredients) {
 		console.log(mainIngredient);
 		console.log(otherIngredients);
 	},
 };
 
+/* 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
