b1b60f6933d944f2ab4a930231eb9ebec23ffcf0
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-19 02:47:13 +0100

    for-of loop

--- script.js
+++ script.js
@@ -52,6 +52,12 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
 
+for (const item of menu.entries()) {
+	console.log(item);
+}
+
+// console.log(...menu.entries());
+
 /////////////////////////////////
 // Coding challenge #1
 
