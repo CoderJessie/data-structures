c4adc3b1a49f0b5e58fab270c8323afa5d4f22d6
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-23 23:09:15 +0100

    map iteration

--- script.js
+++ script.js
@@ -69,10 +69,18 @@ console.log(question.get('question'));
 for (const [key, value] of question) {
 	if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
 }
-const answer = Number(prompt('Your answer'));
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
 console.log(answer);
 
 console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log(...question);
+// console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+
 /*
 //////////////////////////////////////
 // Maps: Fundamentals
