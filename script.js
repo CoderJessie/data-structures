6575f2911511235424bb1d7067a4bb42dd29a67a
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-23 17:56:52 +0100

    map iteration

--- script.js
+++ script.js
@@ -48,6 +48,34 @@ const restaurant = {
 	},
 };
 
+const question = new Map([
+	['question', 'What is the best programming language in the world?'],
+	[1, 'C'],
+	[2, 'Java'],
+	[3, 'JavaScript'],
+	['correct', 3],
+	[true, 'Correct!'],
+	[false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert Object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+	if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+/*
+//////////////////////////////////////
+// Maps: Fundamentals
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
@@ -79,7 +107,6 @@ console.log(rest.size);
 
 console.log(rest.get(arr));
 
-/*
 /////////////////////////////////////
 // Sets
 const ordersSet = new Set([
