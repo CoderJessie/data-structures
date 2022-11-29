eddb4de00c80a579a797449d6364970672ac155e
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-29 13:41:17 +0100

    strings

--- script.js
+++ script.js
@@ -79,7 +79,34 @@ console.log(priceUS);
 const announcement =
 	'All passengers come to boarding door 23. Boarding door 23!';
 
+console.log(announcement.replace('door', 'gate'));
 console.log(announcement.replaceAll('door', 'gate'));
+// console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans Method
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+	console.log('Part of the NEW Airbus family');
+}
+
+// Practice Exercise
+const checkBaggage = function (items) {
+	const baggage = items.toLowerCase();
+
+	if (baggage.includes('knife') || baggage.includes('gun')) {
+		console.log('You are not allowed on board');
+	} else {
+		console.log('Welcome aboard!');
+	}
+};
+
+checkBaggage('I have a laptop, some Food and a pocket knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
 
 /*
 /////////////////////////////////////
