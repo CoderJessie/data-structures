ae5c483c677ff1cb8bfdd2110fda250633531c51
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-30 19:26:46 +0100

    code challenge #4

--- script.js
+++ script.js
@@ -48,6 +48,45 @@ const restaurant = {
 	},
 };
 
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textare inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA(pasted to textarea)
+underscore_case
+first_name
+Some_Variable
+	calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 seperate console.log outputs)
+underscoreCase
+firstName
+someVariable
+calculateAge
+delayedDeparture
+
+HINT 1: Remember which character defines a new line in the textarea
+HINT 2: The soluton only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the 'tick sign'. Tackle that only after you have the variable name conversion working
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK!
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const text = document.querySelector('textarea').value;
+
+/*
+//////////////////////////////////////////
+// Working with strings - Part 3s
 // Split and Join
 console.log('a+very+nice+string'.split('+'));
 console.log('Jonas Schmedtmann'.split(' '));
@@ -98,7 +137,6 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
-/*
 ////////////////////////////////////
 // Working With Strings - Part 2
 
