c018b0d10b3a112b401f5d7c0d3b9c90d92fe234
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-30 19:28:08 +0100

    challenge #4

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ GOOD LUCK!
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
-const text = document.querySelector('textarea').value;
+const text = document.querySelector('texrea').value;
 
 /*
 //////////////////////////////////////////
