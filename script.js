556d854ce2abddbed9a6fd4bfcf6597fd2484b47
Ogbondah Jessica <ogbondahj@gmail.com>
2022-12-03 02:54:14 +0100

    challenge #4

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const restaurant = {
 /*
 Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
 
-The input will come from a textare inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
 
 THIS TEST DATA(pasted to textarea)
 underscore_case
@@ -72,7 +72,7 @@ delayedDeparture				✅✅✅✅✅
 
 HINT 1: Remember which character defines a new line in the textarea
 HINT 2: The soluton only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the 'tick sign'. Tackle that only after you have the variable name conversion working
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working
 HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
 
 Afterwards, test with your own test data!
@@ -82,7 +82,22 @@ GOOD LUCK!
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
-const text = document.querySelector('textarea').value;
+
+document.querySelector('button').addEventListener('click', function () {
+	const text = document.querySelector('textarea').value;
+	const rows = text.split('\n');
+	console.log(rows);
+
+	for (const row of rows) {
+		const [first, second] = row.toLowerCase().trim().split('_');
+		console.log(row, first, second);
+		const output = `${first}${second.replace(
+			second[0],
+			second[0].toUpperCase()
+		)}`;
+		console.log(output);
+	}
+});
 
 /*
 //////////////////////////////////////////
